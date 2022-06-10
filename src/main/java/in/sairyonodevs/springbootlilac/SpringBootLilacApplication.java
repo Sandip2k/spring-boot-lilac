c582bca333b54c37cf1a1d52e0f34aa92adabbc2
Sandip Murmu <sandipmurmusm@gmail.com>
2022-06-10 20:11:49 +0530

    initial commit

---
+++ src/main/java/in/sairyonodevs/springbootlilac/SpringBootLilacApplication.java
@@ -0,0 +1,13 @@
+package in.sairyonodevs.springbootlilac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLilacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLilacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/sairyonodevs/springbootlilac/SpringBootLilacApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sairyonodevs.springbootlilac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLilacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
